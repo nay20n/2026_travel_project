02af29cb18fe7c1cf4748591135d70a5a89c791d
cjsqldbtn <cjsqldbtn@gmail.com>
2026-05-13 10:46:47 +0900

    MODIFY: MemberDao

--- src/main/java/travel_project/MemberDao.java
+++ src/main/java/travel_project/MemberDao.java
@@ -9,10 +9,10 @@ public class MemberDao {
 	
 	/** HA-1. 프로필 이미지 조회 (p.2 / 메인페이지)
 		input : member_id (회원 아이디)
-		output : profile_img(프로필 이미지) */
+		output : profileImg(프로필 이미지) */
 	String getProfileImage(int memberId) throws Exception {
-		String driver = "oracle.jdbc.driver.OracleDriver";//어떤 데이터베이스?
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";//DB의 위치 (오라클의 위치)
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String dbId = "ab";
 		String dbPw = "12345";
 		
@@ -25,11 +25,11 @@ public class MemberDao {
 		
 		ResultSet rs = pstmt.executeQuery();
 		if(rs.next()) {			
-			String profileImage = rs.getString("profile_img");
+			String profileImg = rs.getString("profile_img");
 			rs.close();
 			pstmt.close();
 			conn.close();
-			return profileImage;
+			return profileImg;
 		}else {
 			rs.close();
 			pstmt.close();
@@ -39,11 +39,11 @@ public class MemberDao {
 	}
 	
 	/** HA-4 이메일 조회 (p.3 / 로그인+간편가입 팝업, p.5 / 정보수정1)
-	 	input : 이메일 
+	 	input : 이메일(email)
 	 	output : false(결과없음), true(있음)*/
 	boolean isExistEmail(String email) throws Exception {
-		String driver = "oracle.jdbc.driver.OracleDriver";// 어떤 데이터베이스?
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";// DB의 위치 (오라클의 위치)
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String dbId = "ab";
 		String dbPw = "12345";
 
@@ -67,11 +67,11 @@ public class MemberDao {
 	}
 	
 	/** HA-5 이메일, 비밀번호 조회 (p.3 / 로그인+간편가입 팝업, p.5 / 정보수정1)
-		input : 이메일, 비밀번호  
+		input : 이메일(email), 비밀번호(pw)  
 		output : false(실패), true(성공)*/
 	boolean canLogin(String email, String pw) throws Exception {
-		String driver = "oracle.jdbc.driver.OracleDriver";// 어떤 데이터베이스?
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";// DB의 위치 (오라클의 위치)
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String dbId = "ab";
 		String dbPw = "12345";
 
@@ -96,12 +96,42 @@ public class MemberDao {
 		return false;
 	}
 
+	/** HA-6 비밀번호 재설정 인증코드 수정 (p.5 / 정보수정1)
+		input : 이메일(email)
+		output : - */
+	void updateKey(String email) throws Exception {
+		// 6자리 대문자, 소문자, 숫자로 이루어진 랜덤 key
+		StringBuilder sb = new StringBuilder();
+		while(sb.length()<6) {
+			int temp = (int)(Math.random()*75) + 48;
+			if(temp<58||(temp>64&&temp<91)||(temp>96)) sb.append((char)temp);
+		}
+		
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+		
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+		
+		String sql = "UPDATE members SET key =?, ex_date = SYSDATE+1/24/6 WHERE email = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, sb.toString());
+		pstmt.setString(2, email);
+		
+		pstmt.executeUpdate();
+		
+		pstmt.close();
+		conn.close();
+	}
+	
 	/** HA-7 인증코드, 만료일시 조회 (p.5 / 정보수정1)
-		input : 인증키  
+		input : 인증키(key)  
 		output : false(실패), true(성공)*/
 	boolean isValidCode(String key) throws Exception {
-		String driver = "oracle.jdbc.driver.OracleDriver";// 어떤 데이터베이스?
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";// DB의 위치 (오라클의 위치)
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String dbId = "ab";
 		String dbPw = "12345";
 
@@ -123,30 +153,24 @@ public class MemberDao {
 		if (count==1)return true;
 		return false;
 	}
-
-	/** HA-6 비밀번호 재설정 인증코드 수정 (p.5 / 정보수정1)
-		input : 이메일
+	
+	/** HA-8 회원 삽입 (p.3 / 로그인+간편가입 팝업)
+		input : 이메일(email), 비밀번호(pw)  
 		output : - */
-	void updateKey(String email) throws Exception {
-		// 6자리 대문자, 소문자, 숫자로 이루어진 랜덤 key
-		StringBuilder sb = new StringBuilder();
-		while(sb.length()<6) {
-			int temp = (int)(Math.random()*75) + 48;
-            if(temp<58||(temp>64&&temp<91)||(temp>96)) sb.append((char)temp);
-		}
-
-		String driver = "oracle.jdbc.driver.OracleDriver";// 어떤 데이터베이스?
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";// DB의 위치 (오라클의 위치)
+	void addMember(String email, String pw) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String dbId = "ab";
 		String dbPw = "12345";
-
+		
 		Class.forName(driver);
 		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
-
-		String sql = "UPDATE members SET key =?, ex_date = SYSDATE+1/24/6 WHERE email = ?";
+		
+		String sql = "INSERT INTO members(member_id, email, nick_name, pw)"
+				+ " VALUES(seq_member.nextval, ?, '익명 사용자', ?)";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, sb.toString());
-		pstmt.setString(2, email);
+		pstmt.setString(1, email);
+		pstmt.setString(2, pw);
 		
 		pstmt.executeUpdate();
 		
@@ -154,10 +178,35 @@ public class MemberDao {
 		conn.close();
 	}
 	
+	/** HA-9 프로필이미지 수정 (p.6 / 정보수정2)
+		input : 회원아이디(memberId), 새이미지파일(profileImg)  
+		output : - */
+	void modifyProfileImg(String memberId, String profileImg) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+				
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+				
+		String sql = "UPDATE members SET profile_img = ? WHERE member_id = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, profileImg);
+		pstmt.setString(2, memberId);
+				
+		pstmt.executeUpdate();
+				
+		pstmt.close();
+		conn.close();
+	}
+	
+	
 	public static void main(String[] args) throws Exception {
 		MemberDao a = new MemberDao();
 		System.out.println();
-		a.updateKey("cjsqldbtn@gmail.com");
+		a.modifyProfileImg("100","새로운이미지로변경");
+		System.out.println("정상종료");
 	}
 
 }
