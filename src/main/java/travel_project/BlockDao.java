2f5e4a0fb8e4d2a768bad4eee6833b0f9bccca1b
cjsqldbtn <cjsqldbtn@gmail.com>
2026-05-14 17:56:06 +0900

    MODIFY: BlockDao, PlaceDao, BlockDto ADD: BlockInfoDto

--- src/main/java/travel_project/BlockDao.java
+++ src/main/java/travel_project/BlockDao.java
@@ -1,10 +1,332 @@
 package travel_project;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class BlockDao {
+	/** HA-20 블록 삽입 (p.11 / 일정표-메인(주 단위))
+		input : 게시글번호(bno), 시작시간(startTime), 끝시간(endTime) 
+		output : 블록인덱스(blockIdx)
+		주의 : -1 이 돌아왔다면 문제발생*/
+	int addBlock(int bno, String startTime, String endTime) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+				
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+				
+		String sql = "INSERT INTO blocks(block_idx, bno, start_time, end_time, checked_ai, color_idx)"
+				+ " VALUES(seq_block.nextval, ?, TO_DATE(?, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE(?, 'YYYY-MM-DD HH24:MI:SS'), '0', 1)";
+		PreparedStatement pstmt = conn.prepareStatement(sql, new String[] {"block_idx"});
+		pstmt.setInt(1, bno);
+		pstmt.setString(2, startTime);
+		pstmt.setString(3, endTime);
+		
+		pstmt.executeUpdate();
+		
+		ResultSet rs = pstmt.getGeneratedKeys();
+		int blockIdx = -1;
+		if(rs.next()) blockIdx = rs.getInt(1);
+		
+		rs.close();
+		pstmt.close();
+		conn.close();
+		return blockIdx;
+	}
+	
+	/** HA-21 블록 장소 수정 (p.11 / 일정표-메인(주 단위))
+		input : 블록인덱스(blockIdx), 장소아이디(placeId)
+		output : - */
+	void addBlockPlace(int blockIdx, String placeId) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+				
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+				
+		String sql = "UPDATE  blocks SET place_id = ? WHERE block_idx = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, placeId);
+		pstmt.setInt(2, blockIdx);
+				
+		pstmt.executeUpdate();
+				
+		pstmt.close();
+		conn.close();
+	}
+	
+	/** HA-22 블록 삭제 (p.11 / 일정표-메인(주 단위))
+		input : 블록인덱스(block_idx)
+		output : - */
+	void delBlock(int blockIdx) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+				
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+				
+		String sql = "DELETE FROM blocks WHERE block_idx = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, blockIdx);
+				
+		pstmt.executeUpdate();
+				
+		pstmt.close();
+		conn.close();
+	}
+	
+	/** HA-23 블록 색깔 수정 (p.11 / 일정표-메인(주 단위))
+		input : 블록인덱스(blockIdx), 색 인덱스(colorIdx)
+		output : - */
+	void modifyBlockColor(int blockIdx, int colorIdx) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
 
+		String sql = "UPDATE  blocks SET color_idx = ? WHERE block_idx = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, colorIdx);
+		pstmt.setInt(2, blockIdx);
+
+		pstmt.executeUpdate();
+
+		pstmt.close();
+		conn.close();
+	}
+	
+	/** HA-24 블록 시간 수정 (p.11 / 일정표-메인(주 단위))
+		input : 블록인덱스(blockIdx), 시작시간(StartTime), 끝시간(endTime)  
+		output : - */
+	void modifyBlockTime(int blockIdx, String startTime, String endTime) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+
+		String sql = "UPDATE  blocks SET start_time = TO_DATE(?, 'YYYY-MM-DD HH24:MI:SS') , end_time = TO_DATE(?, 'YYYY-MM-DD HH24:MI:SS')" 
+				+ " WHERE block_idx = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, startTime);
+		pstmt.setString(2, endTime);
+		pstmt.setInt(3, blockIdx);
+
+		pstmt.executeUpdate();
+
+		pstmt.close();
+		conn.close();
+	}
+	
+	/** HA-25 블록 AI 반영여부 수정 (p.11 / 일정표-메인(주 단위))
+		input : 블록인덱스(blockIdx), 반영여부(isCheckedAi)
+		output : - */
+	void modifyBlockCheckedAi(int blockIdx, boolean isCheckedAi) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+
+		String sql = "UPDATE  blocks SET checked_ai = ? WHERE block_idx = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, isCheckedAi ? 1 : 0);
+		pstmt.setInt(2, blockIdx);
+
+		pstmt.executeUpdate();
+
+		pstmt.close();
+		conn.close();
+	}
+	
+	/** HA-26 블록 팝업창 정보 조회 (p.11 / 일정표-메인(주 단위))
+		input : 블록인덱스(blockIdx) 
+		output : 해당 블록의 BlockDto */
+	BlockInfoDto viewBlockDetails(int blockIdx) throws Exception {
+		BlockInfoDto b = null;
+		
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+
+		String sql = "SELECT b.start_time, b.end_time, b.checked_ai,"
+				+ " b.color_idx, c.color_code, p.name, p.category, p.address, p.lat, p.lng"
+				+ "	FROM blocks b LEFT OUTER JOIN places p"
+				+ "	ON b.place_id = p.place_id"
+				+ "	INNER JOIN colors c ON b.color_idx = c.color_idx"
+				+ "	WHERE block_idx = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, blockIdx);
+
+		ResultSet rs = pstmt.executeQuery();
+		if(rs.next()) {
+			String startTime = rs.getString("start_time");
+			String endTime = rs.getString("end_time");
+			boolean isCheckedAi = rs.getInt("checked_ai")==1;
+			int colorIdx = rs.getInt("color_idx");
+			String colorCode = rs.getString("color_code");
+			String name = rs.getString("name");
+			String category = rs.getString("category");
+			String address = rs.getString("address");
+			double lat = rs.getDouble("lat");
+			double lng = rs.getDouble("lng");
+			b = new BlockInfoDto(startTime, endTime, isCheckedAi, colorIdx, colorCode, name, category, address, lat, lng);
+		}
+				
+		rs.close();
+		pstmt.close();
+		conn.close();
+		return b;
+	}
+	
+	/** HA-27 게시글의 블록 정보 전체 조회 (p.11 / 일정표-메인(주 단위))
+		input : 게시글번호
+		output : List<BlockDto> 
+		주의 : 장소가 없는 경우 위도, 경도에 0이 들어감 */
+	List<BlockDto> showAllBlocks(int bno) throws Exception {
+		List<BlockDto> list = new ArrayList<>();
+		
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+
+		String sql = "SELECT b.block_idx, b.start_time, b.end_time, b.checked_ai, b.travel_time,"
+				+ " b.color_idx, c.color_code, p.name, lat, lng"
+				+ "	FROM blocks b LEFT OUTER JOIN places p ON b.place_id = p.place_id"
+				+ " INNER JOIN colors c ON b.color_idx = c.color_idx"
+				+ "	WHERE b.bno = ?"
+				+ "	ORDER BY start_time";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, bno);
+
+		ResultSet rs = pstmt.executeQuery();
+		while(rs.next()) {
+			int blockIdx = rs.getInt("block_idx");
+			String startTime = rs.getString("start_time");
+			String endTime = rs.getString("end_time");
+			boolean isCheckedAi = rs.getInt("checked_ai")==1;
+			int travelTime = rs.getInt("travel_time");
+			int colorIdx = rs.getInt("color_idx");
+			String colorCode = rs.getString("color_code");
+			String name = rs.getString("name");
+			double lat = rs.getDouble("lat");
+			double lng = rs.getDouble("lng");
+			BlockDto b = new BlockDto(blockIdx, startTime, endTime, isCheckedAi, colorIdx, travelTime, colorCode, name, colorCode, name, lat, lng);
+			list.add(b);
+		}
+				
+		rs.close();
+		pstmt.close();
+		conn.close();
+		return list;
+	}
+	public static void main(String[] args) throws Exception {
+		BlockDao b = new BlockDao();
+		Scanner sc = new Scanner(System.in);
+		
+		// 기본 설정값
+		int bno = 100;
+		String placeId = "ChIJofoWUQCNaDURDqIZjAjYMU8";
+		
+		// HA-20
+//		String startTime = "2026-03-21 14:00:00";
+//		String endTime = "2026-03-21 15:00:00";
+//		System.out.println(b.addBlock(bno, startTime, endTime));
+		
+		// HA-21
+//		System.out.print("변경할 블록 인덱스: ");
+//		int blockIdx = sc.nextInt();
+//		sc.nextLine();
+//		b.addBlockPlace(blockIdx, placeId);
+		
+		// HA-22
+//		System.out.print("변경할 블록 인덱스: ");
+//		int blockIdx = sc.nextInt();
+//		sc.nextLine();
+//		b.delBlock(blockIdx);
+		
+		// HA-23
+//		System.out.print("변경할 블록 인덱스: ");
+//		int blockIdx = sc.nextInt();
+//		sc.nextLine();
+//		System.out.print("색 인덱스: ");
+//		int colorIdx = sc.nextInt();
+//		sc.nextLine();
+//		b.modifyBlockColor(blockIdx, colorIdx);
+		
+		// HA-24
+//		System.out.print("변경할 블록 인덱스: ");
+//		int blockIdx = sc.nextInt();
+//		sc.nextLine();
+//		String startTime = "2026-03-22 14:00:00";
+//		String endTime = "2026-03-24 15:00:00";
+//		b.modifyBlockTime(blockIdx, startTime, endTime);
+		
+		// HA-25
+//		System.out.print("변경할 블록 인덱스: ");
+//		int blockIdx = sc.nextInt();
+//		sc.nextLine();
+//		b.modifyBlockCheckedAi(blockIdx, true);
+		
+		// HA-26
+//		System.out.print("조회할 블록 인덱스: ");
+//		int blockIdx = sc.nextInt();
+//		sc.nextLine();
+//		BlockInfoDto bl = b.viewBlockDetails(blockIdx);
+//		System.out.println("색상인덱스: " + bl.colorIdx);
+//		System.out.println("색상 코드: " + bl.colorCode);
+//		System.out.println("시작 시간: " + bl.startTime);
+//		System.out.println("끝 시간: " + bl.endTime);
+//		System.out.println("위도: " + bl.lat);
+//		System.out.println("경도: " + bl.lng);
+//		System.out.println("ai 반영 여부: " + bl.isCheckedAi);
+//		System.out.println("장소: " + bl.name);
+//		System.out.println("카테고리: " + bl.category);
+//		System.out.println("주소: " + bl.address);
+		
+		// HA-27
+//		List<BlockDto> bd = b.showAllBlocks(bno);
+//		for(int i=0;i<bd.size();i++) {
+//			BlockDto bl = bd.get(i);
+//			System.out.println("색 인덱스: " + bl.colorIdx);
+//			System.out.println("색상 코드: " + bl.colorCode);
+//			System.out.println("블럭 인덱스: " + bl.blockIdx);
+//			System.out.println("블럭 시간: " + bl.startTime + "~" + bl.endTime);
+//			System.out.println("ai 반영여부: " + bl.isCheckedAi);
+//			System.out.println("이동시간: " + bl.travelTime);
+//			System.out.println("장소이름: " + bl.name);
+//			System.out.println("위도, 경도: " + bl.lat + ", " + bl.lng);
+//		}
+		
+		// 정상종료
+		sc.close();
+		System.out.println("정상종료");
 	}
 
 }
