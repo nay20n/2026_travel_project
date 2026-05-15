04b37b2c60b3041bf8264149ccb657265b938d06
nayeon <nayeon@localhost>
2026-05-15 17:01:48 +0900

    MODIFY : AiBlockeDao, CommentDao, MemberDao, PlaceDao
    ADD: AiBlockDto

--- src/main/java/travel_project/MemberDao.java
+++ src/main/java/travel_project/MemberDao.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemberDao {
 	
@@ -276,9 +278,251 @@ public class MemberDao {
 		pstmt.close();
 		conn.close();
 	}
+	
+	/** NY-1. 내 게시글들 조회, 공동작업자부분도 띄어줘야함 (p.4 / 마이페이지)
+		input : 로그인한 사람의 member_id, 페이지번호
+		output : 내 일정들 나열 (List<MypageDto>) */
+	List<MypageDto> getMyBoard(int memberId, int page) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+	
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+	
+		String sql = "SELECT b.bno \"게시글 번호\", TRUNC(SYSDATE-start_date) \"디데이\", title \"일정 이름\", nick_name \"작성자\", "
+				+ "(SELECT COUNT(*) FROM like_boards lb WHERE lb.member_id = ? AND lb.bno = b.bno)\"찜 유무\", "
+				+ "(SELECT COUNT(*)FROM like_boards lb WHERE lb.bno = b.bno) \"찜 개수\" "
+				+ "FROM members m "
+				+ "INNER JOIN boards b ON m.member_id = b.writer_id "
+				+ "WHERE b.writer_id = ? OR b.share_user_id = ? "
+				+ "ORDER BY final_date DESC "; 
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, memberId);
+		pstmt.setInt(2, memberId);
+		pstmt.setInt(3, memberId);
+	
+		//3. 결과테이블 : “ResultSet객체”
+		ResultSet rs = pstmt.executeQuery();
+		List<MypageDto> mypageDtos = new ArrayList<>();
+	    while (rs.next()) { // 결과 행으로 이동 (필수!)
+	    	int bno = rs.getInt("게시글 번호");
+	    	int dDayInt = rs.getInt("디데이");
+	    	String dDay = "";
+	    	if(dDayInt > 0) {
+	    		dDay = "D+" + dDayInt;
+	    	} else if (dDayInt < 0 ) {
+	    		dDay = "D-" + Math.abs(dDayInt);
+	    	} else { dDay = "D-day"; }
+	    	String title = rs.getString("일정 이름");
+	    	String nickname = rs.getString("작성자");
+	    	boolean isLiked = (rs.getInt("찜 유무")==1 ? true : false);
+	    	int likeCnt = rs.getInt("찜 개수");
+	    	MypageDto mypageDto = new MypageDto(bno, dDay, title, nickname, isLiked, likeCnt);
+	    	mypageDtos.add(mypageDto);
+	    }
+	
+	    rs.close();
+		pstmt.close();
+		conn.close();
+		
+		return mypageDtos;
+	}
+	
+	/** NY-2. 내가 찜한 일정들 조회 (p.4 / 마이페이지)
+		input : 로그인한 사람의 member_id, 페이지 번호 
+		output : 로그인한 사람이 찜한 게시물 조회 (List<MyPageDto>) */
+	List<MypageDto> getLikedBoard(int memberId, int page) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+	
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+	
+		String sql = "SELECT  b.bno \"찜한 게시글 번호\", title \"일정이름\", nick_name \"작성자\" , "
+				+ "(SELECT COUNT(*) FROM like_boards lb WHERE lb.member_id = ? AND lb.bno = b.bno)\"찜 유무\", "
+				+ "(SELECT COUNT(*) FROM like_boards lb WHERE lb.bno = b.bno) \"찜 개수\" "
+				+ "FROM like_boards lb "
+				+ "INNER JOIN boards b ON lb.bno = b.bno "
+				+ "INNER JOIN members m ON b.writer_id = m.member_id "
+				+ "WHERE lb.member_id = ? "
+				+ "ORDER BY like_time DESC " ;
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, memberId);
+		pstmt.setInt(2, memberId);
+	
+		ResultSet rs = pstmt.executeQuery();
+		List<MypageDto> mypageDtos = new ArrayList<>();
+	    while (rs.next()) { 
+	    	int bno = rs.getInt("찜한 게시글 번호");
+	    	String dDay = null;
+	    	String title = rs.getString("일정이름");
+	    	String nickname = rs.getString("작성자");
+	    	boolean isLiked = (rs.getInt("찜 유무")==1 ? true : false);
+	    	int likeCnt = rs.getInt("찜 개수");
+	    	MypageDto mypageDto = new MypageDto(bno, dDay, title, nickname, isLiked, likeCnt);
+	    	mypageDtos.add(mypageDto);
+	    }
+	
+	    rs.close();
+		pstmt.close();
+		conn.close();
+		
+		return mypageDtos;
+	}
+
+	/** NY-3. 내가 댓글 단 일정 조회 (p.4 / 마이페이지) 
+		input : 로그인한 사람의 member_id, 페이지 번호
+		output : 로그인한 사람이 댓글을 단 게시물 조회  (List<MyPageDto>) */
+	List<MypageDto> getCommentBoard(int memberId, int page) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+	
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+	
+		String sql = "SELECT b.bno \"댓글을 단 게시글 번호\","
+				+ " title \"일정이름\", "
+				+ "nick_name \"작성자\", "
+				+ "(SELECT COUNT(*) FROM like_boards lb WHERE lb.member_id = ? AND lb.bno = b.bno) \"찜 유무\", "
+				+ "(SELECT COUNT(*) FROM like_boards lb WHERE lb.bno = b.bno) \"찜 개수\" "
+				+ "FROM members m "
+				+ "INNER JOIN boards b ON m.member_id = b.writer_id "
+				+ "WHERE (SELECT MAX(c.final_date) FROM comments c WHERE c.bno = b.bno  AND writer_id = ?) IS NOT NULL "
+				+ "ORDER BY (SELECT MAX(c.final_date) FROM comments c WHERE c.bno = b.bno  AND writer_id = ?)";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, memberId);
+		pstmt.setInt(2, memberId);
+		pstmt.setInt(3, memberId);
+	
+		//3. 결과테이블 : “ResultSet객체”
+		ResultSet rs = pstmt.executeQuery();
+		List<MypageDto> mypageDtos = new ArrayList<>();
+	    while (rs.next()) { // 결과 행으로 이동 (필수!)
+	    	int bno = rs.getInt("댓글을 단 게시글 번호");
+	    	String dDay = null;
+	    	String title = rs.getString("일정이름");
+	    	String nickname = rs.getString("작성자");
+	    	boolean isLiked = (rs.getInt("찜 유무")==1 ? true : false);
+	    	int likeCnt = rs.getInt("찜 개수");
+	    	
+	    	MypageDto mypageDto = new MypageDto(bno, dDay, title, nickname, isLiked, likeCnt);
+	    	mypageDtos.add(mypageDto);
+	    }
+	
+	    rs.close();
+		pstmt.close();
+		conn.close();
+		
+		return mypageDtos;
+	}
+	
+	/** NY-4. 프로필 & 활동한 내용 카운트 조회 (p.4 / 마이페이지) -member
+		ProfileDto(프로필 사진, 닉네임, 작성한 일정 수, 찜한 수, 댓글 쓴 수)
+		input : 로그인한 사람의 member_id
+		output : ProfileDto*/
+	ProfileDto getMemberProfile(int memberId) throws Exception {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String dbId = "ab";
+		String dbPw = "12345";
+	
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+	
+		String sql = "SELECT profile_img \"프로필 사진\", nick_name \"닉네임\", "
+				+ "	(SELECT COUNT(*) FROM boards WHERE writer_id = ? OR share_user_id = ?) \"내 일정 수\", "
+				+ "	(SELECT COUNT(*) FROM like_boards WHERE member_id = ?) \"찜한 수\", "
+				+ "(SELECT COUNT(*) FROM comments WHERE writer_id = ?) \"댓글 쓴 수\" "
+				+ "FROM members "
+				+ "WHERE member_id = ? ";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, memberId);
+		pstmt.setInt(2, memberId);
+		pstmt.setInt(3, memberId);
+		pstmt.setInt(4, memberId);
+		pstmt.setInt(5, memberId);
+	
+		//3. 결과테이블 : “ResultSet객체”
+		ResultSet rs = pstmt.executeQuery();
+		ProfileDto profileDto = null;
+	    if (rs.next()) { 
+	    	
+	    	String profile = rs.getString("프로필 사진");
+	    	String nickName = rs.getString("닉네임");
+	    	int myTavelCnt = rs.getInt("내 일정 수");
+	    	int myLikeCnt = rs.getInt("찜한 수");
+	    	int myCommentCnt = rs.getInt("댓글 쓴 수");
+	    	
+	    	profileDto = new ProfileDto(profile, nickName, myTavelCnt, myLikeCnt, myCommentCnt);
+	    }
+	
+	    rs.close();
+		pstmt.close();
+		conn.close();
+		
+		return profileDto;
+	}
 
 	public static void main(String[] args) throws Exception {
 		MemberDao a = new MemberDao();
+		
+		//NY-1.
+//		int bno = 1;
+//		int page = 1;
+//		List <MypageDto> list = a.getMyBoard(bno, page);
+//		for(int i=0; i<list.size(); i++) {
+//			System.out.println("게시글 번호 : " + list.get(i).bno);
+//			System.out.println("디데이 : " + list.get(i).dDay);
+//			System.out.println("일정 이름 : " + list.get(i).title);
+//			System.out.println("작성자 : " + list.get(i).writerNick);
+//			System.out.println("찜 유무 : " + list.get(i).isLiked);
+//			System.out.println("찜 개수 : " + list.get(i).likeBoardCnt);
+//			System.out.println();
+//		}
+		
+		//NY-2.
+//		int bno = 1;
+//		int page = 1;
+//		List <MypageDto> list = a.getLikedBoard(bno, page);
+//		for(int i=0; i<list.size(); i++) {
+//			System.out.println("게시글 번호 : " + list.get(i).bno);
+//			System.out.println("디데이 : " + list.get(i).dDay);
+//			System.out.println("일정 이름 : " + list.get(i).title);
+//			System.out.println("작성자 : " + list.get(i).writerNick);
+//			System.out.println("찜 유무 : " + list.get(i).isLiked);
+//			System.out.println("찜 개수 : " + list.get(i).likeBoardCnt);
+//			System.out.println();
+//		}
+		
+		//NY-3.
+//		int bno = 1;
+//		int page = 1;
+//		List <MypageDto> list = a.getCommentBoard(bno, page);
+//		for(int i=0; i<list.size(); i++) {
+//			System.out.println("게시글 번호 : " + list.get(i).bno);
+//			System.out.println("디데이 : " + list.get(i).dDay);
+//			System.out.println("일정 이름 : " + list.get(i).title);
+//			System.out.println("작성자 : " + list.get(i).writerNick);
+//			System.out.println("찜 유무 : " + list.get(i).isLiked);
+//			System.out.println("찜 개수 : " + list.get(i).likeBoardCnt);
+//			System.out.println();
+//		}
+		
+		//NY-4.
+//		int memberId = 1;
+//		ProfileDto profileDto = a.getMemberProfile(memberId);
+//		System.out.println("프로필 사진 : " + profileDto.profile);
+//		System.out.println("닉네임 : " + profileDto.nickName);
+//		System.out.println("내 일정 수 : " + profileDto.myTavelCnt);
+//		System.out.println("찜한 수 : " + profileDto.myLikeCnt);
+//		System.out.println("댓글 쓴 수 : " + profileDto.myCommentCnt);
+		
 		System.out.println();
 		System.out.println("정상종료");
 	}
