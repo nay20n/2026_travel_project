50ca3d394739ab69d5bddad2fc32c24cfba49b0e
cjsqldbtn <cjsqldbtn@gmail.com>
2026-06-17 18:01:53 +0900

    Modify: classname

--- src/main/webapp/js/MainPage.js
+++ src/main/webapp/js/MainPage.js
@@ -1,16 +1,20 @@
 $(function() {
 	// 메인로고 누르면 메인화면으로
-	$(".mainlogo").click(function() {
+	$(".mainLogo").click(function() {
 		location.href="MainPage.html";
 	});
 	// 로그인 팝업
 	$(".login").click(function() {
-		$(".popup-container").attr("style","display: block");
+		$(".popupContainer").attr("style","display: block");
 		$(".loginpop").attr("style","display: block");
 	});
 	// 팝업창 닫기
-	$(".popup-container> div > svg:nth-child(1)").click(function() {
-		$(".popup-container").attr("style","display: none");
-		$(".popup-content").attr("style","display: none");
+	$(".popupContainer> div > svg:nth-child(1)").click(function() {
+		$(".popupContainer").attr("style","display: none");
+		$(".popupContent").attr("style","display: none");
+	});
+	// 비밀번호 재설정 페이지로 이동
+	$(".popupContainer>div:nth-child(1)>div:nth-child(3)>div:nth-child(4)>span:nth-child(2)").click(function() {
+		location.href="ResetPw.html";
 	});
 });
